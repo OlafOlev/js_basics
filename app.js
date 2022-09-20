10becfb96b3f349ba8994b8515db97651d31037c
olaf <oalfolev@gmail.com>
2022-09-20 11:30:20 +0300

    Create project files

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 console.log("Hello World!")
+//string
+let name = "Olaf Olev Alakivi"
+console.log(name)
+console.log(typeof name)
+
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(percent)
+console.log(typeof percent)
+
+let isHere = true//false
+console.log(isHere)
+console.log(typeof isHere)
+
+//object
+const person = {
+    firstname: "Olaf Olev",
+    surname:"Alakivi"
+}
+console.log(person)
+console.log(typeof person)
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = "ITA21"
+console.log(person)
+
+//null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+//Symbol
+const symbol = Symbol()
+console.log(symbol)
+console.log(typeof symbol)
+
+//undefined
+let test
+console.log(test)
+console.log(typeof test)
+
+
+// //const
+// const surname = "Alakivi"
+//
+// // arrays
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers)
+// numbers.push(6)
+// console.log(numbers)

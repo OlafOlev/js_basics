55ba5569718e03bd8d0e327bfbf1c2a48db73e5f
olaf <oalfolev@gmail.com>
2022-09-21 09:53:05 +0300

    Create project files

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const firstname = "Olaf Olev"
-const surname = "Alakivi"
-const email = "olaf-olev.alakivi@voco.ee"
-const department = "ITA21"
+const numbers1 = [69, 9000, 420, 666, 1]
+const numbers2 = new Array(9000, 420, 666, 1)
 
-let result = '<ul>' +
-    '<li><b>Name:</b>' + firstname + '</li>' +
-    '<li><b>Surname:</b>' + surname + '</li>' +
-    '<li><b>Email:</b>' + email + '</li>' +
-    '<li><b>Department:</b>' + department + '</li>' +
-    '</ul>'
-result = `
-    <ul>
-        <li><b>Name: </b>${firstname}</li>
-        <li><b>Surname: </b>${surname}</li>
-        <li><b>Email: </b>${email}</li>
-        <li><b>Department: </b>${department}</li>
-    </ul>`
+let val
 
-console.log(result)
-document.body.innerHTML = result
+val = numbers1.length
+val = numbers2.length
+
+val = numbers1[1]
+val = numbers1.indexOf(666)
+
+val = Array.isArray(val)
+
+numbers1.push(123)
+numbers1.unshift(100)
+numbers1.shift()
+val = numbers1.concat(numbers2)
+
+
+console.log(numbers1)
+numbers1.sort(function (x,y) {
+    return y - x
+})
+console.log(numbers1)

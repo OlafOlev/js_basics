f5e0e4b0833a3535a8c3e99e8aa290cee4366c2c
olaf <oalfolev@gmail.com>
2022-09-22 13:44:28 +0300

    Yes

--- app.js
+++ app.js
@@ -1,19 +1,74 @@
-const today = new Date()
+/*
+if(function){
+    // if statements
+} else {
+    // else statements
+}
 
-let val
 
-val = today
-val = today.getMonth()
-val = today.getDate()
-val = today.getDay()
-val = today.getFullYear()
-val = today.getHours()
-val = today.getMinutes()
-val = today.getTime()
+== equal -> ===
+!== not equal -> !==
+*/
 
-val = new Date("09/22/2021")
-val = new Date("September  23 2022")
+// let personId = 23;
+//
+// console.log(personId)
+//
+// if(typeof personId === 'undefined') {
+//     console.log('id is not correct')
+// }
+const color1= "yellow"
 
-val.setFullYear(2023)
+if(color1 === "red"){
+    console.log("Stop!")
+} else if(color1 === "yellow"){
+    console.log("Attention!")
+} else if(color1 === "green"){
+    console.log("Go!")
+} else {
+    console.log("Error!")
+}
 
-console.log(val)
+const name = "Kadi"
+const age = "1"
+
+if(age > 0 && age <= 12){
+    console.log(`${name} on laps`)
+} else if(age > 12 && age <= 18){
+    console.log(`${name} on nooruk`)
+} else{
+    console.log(`${name} on täiskasvanu`)
+}
+
+let id = 5
+
+if(id === 100){
+    console.log("Ok")
+} else{
+    console.log("Not")
+}
+
+
+// ? - ternary
+console.log(id === 100 ? "Ok" : "Not")
+
+// switch
+const color = 'blue'
+
+switch (color) {
+    case "red":
+        console.log('Stop!')
+        break;
+    case "yellow":
+        console.log('Attention!')
+        break;
+    case "green":
+        console.log('Go!')
+        break;
+    default:
+        console.log("Error!")
+
+}
+
+
+

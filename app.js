4a5b164a1fa40f1cfae97e24f782a859441d9ad1
olaf <oalfolev@gmail.com>
2022-09-22 13:17:46 +0300

    Data processing

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-const person = {
-    firstname: 'Kadi',
-    surname: 'Tamm',
-    age: 40,
-    email:'kadi.tamm@gmail.com',
-    hobbies: ['tennis', 'korvpall'],
-    address: {
-        city:'võru',
-        county:'Võrumaa'
-    },
-    getBirthYear: function (){
-        return 2022 - this.age
-    }
-}
+const today = new Date()
 
 let val
-val = person
-val = person.firstname
-val = person['surname']
-val = person['hobbies'][1]
-val = person.address.city
-val = person.address['city']
-val = person.getBirthYear()
 
-// for(let i = 0; i< person.hobbies.length; i++){
-//     console.log(person.hobbies[i])
-//     console.log(i)
-// }
+val = today
+val = today.getMonth()
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getMinutes()
+val = today.getTime()
 
-person.hobbies.forEach((hobbie, index) => {
-    console.log(hobbie + " on element indeksiga " + index)
-})
+val = new Date("09/22/2021")
+val = new Date("September  23 2022")
+
+val.setFullYear(2023)
 
 console.log(val)
